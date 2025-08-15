21432ce84d80c3b49b472f30d3cf0f1cffa27897
pc-sam <samurai.h@yahoo.com>
2025-08-15 12:02:03 +0800

    add 2 functions to append the new cats one is in the front the other is at the end

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+   return cats.push("Ralph")
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift("Bob")
+}
